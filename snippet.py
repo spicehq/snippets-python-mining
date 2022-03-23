345a9fc2cd19ec79fc0fcb6f7b864327160d20ad
Corentin <corentin-pro@mail.com>
2022-03-23 16:59:40 +0900

    Add certificate handling

--- snippet.py
+++ snippet.py
@@ -2,8 +2,24 @@
 Getting average mining time per day of week for the latests blocks
 Find more information at https://github.com/spicehq/samples
 '''
+import os
+from pathlib import Path
+import ssl
+import tempfile
+import urllib.request
 
 from pyarrow import flight
+
+# Check for gRPC certificates
+if not (Path(Path.cwd().absolute().anchor) / 'usr' / 'share' / 'grpc' / 'roots.pem').exists():
+    env_name = 'GRPC_DEFAULT_SSL_ROOTS_FILE_PATH'
+    if env_name not in os.environ or not Path(os.environ[env_name]).exists():
+        tls_root_certs = Path(tempfile.gettempdir()) / 'roots.pem'
+        if not Path(tls_root_certs).exists():
+            print('Downloading gRPC certificates')
+            ssl._create_default_https_context = ssl._create_unverified_context
+            urllib.request.urlretrieve('https://pki.google.com/roots.pem', str(tls_root_certs))
+        os.environ[env_name] = str(tls_root_certs)
 
 client = flight.connect('grpc+tls://flight.spiceai.io')
 token_pair = client.authenticate_basic_token('', 'API_KEY')
